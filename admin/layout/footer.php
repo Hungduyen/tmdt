c28fed69f3830cfd70ef199a2a7a3f70623638a2
Hung <nguyentronghung3005@gmail.com>
2022-05-29 20:33:08 +0700

    hungudyen

---
+++ admin/layout/footer.php
@@ -0,0 +1,9 @@
+<div id="footer-wp">
+    <div class="wp-inner">
+        <p id="copyright">2020 © NGUYENTRONGHUNG | LEHUYNHNGUYEN | TRUONGVIETHOANG</p>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ modules/contacts/views/introduceView.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+				<div id="main-content-wp" class="checkout-page">
+    <div class="section" id="breadcrumb-wp">
+        <div class="wp-inner">
+            <div class="section-detail">
+                <ul class="list-item clearfix">
+                    <li>
+                        <a href="?page=home" title="">Trang chủ</a>
+                    </li>
+                    <li>
+                        <a href="" title="">Giới thiệu</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="wrapper" class="wp-inner clearfix">
+        <div class="section" id="customer-info-wp">
+            <div class="section-head">
+                <h1 class="section-title">Giới thiệu</h1>
+                <div>Nhóm 11</div>
+                <div>Lập trình Web nâng cao</div>
+                <div>GVHD: Ngô Lê Quân</div>
+            </div>
+
+        </div>
+        <div class="section" id="order-review-wp">
+            <div class="section-head">
+                <h1 class="section-title">Giới thiệu</h1>
+                <div><span style="color:red">Nguyễn Trọng Hưng: 1911505310225</span></div>
+                <div><span style="color:red">Lê Huỳnh Nguyên: 1911514110113</span></div>
+                <div><span style="color:red">Trương Việt Hoàng: 1911514110220</span></div>
+            </div>
+            <div class="section-detail">
+
+                
+            </div>
+        </div>
+    </div>
+</div>	
+<?php get_footer(); ?>
